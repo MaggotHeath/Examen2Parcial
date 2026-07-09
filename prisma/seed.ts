1afb1daf2ace6083f76bedf96b921e24d67abfee
Mario Daniel Orellana Toro <mardaniel2005@gmail.com>
2026-07-08 22:11:51 -0600

    FixVercel

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient({
   adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
